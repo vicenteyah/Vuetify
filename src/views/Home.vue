0ed8191c9a89adbd7ac72183fb8b40edc602f988
Vicente <vicente.yah.dzul@icloud.com>
2020-04-29 18:40:06 -0500

    v-cards added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
- <v-container>
+ <v-container grid-list-lg>
    <v-layout row wrap>
-     <v-flex  xs12 sm6>
+     <v-flex xs12 sm6>
        <v-card>
-         <v-img src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" height="200px" ></v-img>
+         <v-img src="https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg" width="600" ></v-img>
+         <v-card-title><h2>Pizza</h2></v-card-title>
+         <v-card-text>Lorem, ipsum dolor.</v-card-text>
+         <v-card-actions><v-btn rounded color="info">ordenar</v-btn></v-card-actions>
+       </v-card>
+     </v-flex>
+
+     <v-flex xs12 sm6>
+       <v-card>
+         <v-img src="https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg" width="600" ></v-img>
+         <v-card-title><h2>Pizza</h2></v-card-title>
+         <v-card-text>Lorem, ipsum dolor.</v-card-text>
+         <v-card-actions><v-btn rounded color="info">ordenar</v-btn></v-card-actions>
        </v-card>
      </v-flex>
    </v-layout>
