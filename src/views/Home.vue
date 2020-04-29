1cba4262f9aec39755e98f81879cc8a0eaf872c2
Vicente <vicente.yah.dzul@icloud.com>
2020-04-29 18:30:52 -0500

    new changes added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>hello</h1>
-  </div>
+ <v-container>
+   <v-layout row wrap>
+     <v-flex  xs12 sm6>
+       <v-card>
+         <v-img src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" height="200px" ></v-img>
+       </v-card>
+     </v-flex>
+   </v-layout>
+ </v-container>
 </template>
 
