a4f50178a5c8d251d115285658cf02b8fed0833f
Vicente <vicente.yah.dzul@icloud.com>
2020-04-30 00:06:55 -0500

    v-for directive to get many cards with diferents data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,12 @@
 <template>
  <v-container grid-list-lg>
    <v-layout row wrap>
-     <v-flex xs12 sm6>
-       <v-card>
-         <v-img src="https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg" width="600" ></v-img>
-         <v-card-title><h2>Pizza</h2></v-card-title>
-         <v-card-text>Lorem, ipsum dolor.</v-card-text>
-         <v-card-actions><v-btn rounded color="info">ordenar</v-btn></v-card-actions>
-       </v-card>
-     </v-flex>
-
-     <v-flex xs12 sm6>
-       <v-card>
-         <v-img src="https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg" width="600" ></v-img>
-         <v-card-title><h2>Pizza</h2></v-card-title>
-         <v-card-text>Lorem, ipsum dolor.</v-card-text>
+     <v-flex column wrap xs12 sm6>
+       <v-card class="my-2" v-for="(item,index) in images" :key="index">
+         <v-img :src="images[index].url" width="600" ></v-img>
+         <v-card-title><h2>{{ item.title }}</h2></v-card-title>
+         <v-card-text>{{ item.description }}</v-card-text>
+         <v-card-text>{{ item.price }}</v-card-text>
          <v-card-actions><v-btn rounded color="info">ordenar</v-btn></v-card-actions>
        </v-card>
      </v-flex>
@@ -22,3 +14,15 @@
  </v-container>
 </template>
 
+<script>
+export default {
+  data:() => ({
+    images:[
+      {id:1,url:'https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg',
+      title:'HuastiSpecial',description:'la mejor de nuestro menu diseñada por el huasteco' ,price:'199.99'},
+      {id:2,url:'https://www.recetastv.com/wp-content/uploads/2019/03/pizza-hawaiana.jpg',
+      title:'pizza hawiana', description:'Extra queso para darte gusto a ti',price:'189.99'}
+    ]
+  })
+}
+</script>
