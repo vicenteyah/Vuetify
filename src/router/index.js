0ce17706fd25b583c3b253a63128f5df33398bde
Vicente <vicente.yah.dzul@icloud.com>
2020-05-03 19:56:53 -0500

    many routes were configured

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Login from '../views/Login.vue'
+import Profile from '../views/Profile.vue'
+import Register from '../views/Register.vue'
+import user from '../views/BoardUser.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -17,6 +20,24 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/Login',
+    component: Login
+  },
+  {
+    path:'register',
+    component:Register
+  },
+  {
+    path:'/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path:'/user',
+    name:'user',
+    component: user
   }
 ]
 
