da055d3c340a97ec98b377b3fa676a1bd182fc5a
Vicente <vicente.yah.dzul@icloud.com>
2020-05-03 19:58:44 -0500

    template modified to prove axios and nodejs backed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,12 @@
  <v-container grid-list-lg>
    <v-layout row wrap>
      <v-flex column wrap xs12 sm6>
-       <v-card class="my-2" v-for="(item,index) in images" :key="index">
-         <v-img :src="images[index].url" width="600" ></v-img>
-         <v-card-title><h2>{{ item.title }}</h2></v-card-title>
-         <v-card-text>{{ item.description }}</v-card-text>
-         <v-card-text>{{ item.price }}</v-card-text>
+       <h1>{{ content }}</h1>
+       <v-card class="my-2" v-for="(promotion,index) in promotions" :key="index">
+         <!--<v-img :src="images[index].url" width="600" ></v-img> not working yet--> 
+         <v-card-title><h2>{{ promotion.packagename }}</h2></v-card-title>
+         <v-card-text>Descripcion: {{ promotion.description }}$</v-card-text>
+         <v-card-text>Precio: {{ promotion.price }}$</v-card-text>
          <v-card-actions><v-btn rounded color="info">ordenar</v-btn></v-card-actions>
        </v-card>
      </v-flex>
@@ -15,14 +16,34 @@
 </template>
 
 <script>
+import UserService from '../services/user.service';
+import PromotionsService from '../services/promotions.service';
 export default {
   data:() => ({
-    images:[
-      {id:1,url:'https://images.template.net/wp-content/uploads/2019/03/Featured-Image18.jpg',
-      title:'HuastiSpecial',description:'la mejor de nuestro menu diseñada por el huasteco' ,price:'199.99'},
-      {id:2,url:'https://www.recetastv.com/wp-content/uploads/2019/03/pizza-hawaiana.jpg',
-      title:'pizza hawiana', description:'Extra queso para darte gusto a ti',price:'189.99'}
-    ]
-  })
+    promotions:[],
+    content:''
+  }),
+  mounted() {
+    UserService.getPublicContent().then(
+      response => {
+        this.content = response.data;
+      },
+      error => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+    PromotionsService.getpromotions().then(
+      response => {
+        console.log(response.data,'this array')
+        this.promotions = response.data;
+
+      },err =>{
+        console.log(err);
+      }
+    );
+  }
 }
 </script>
