553dffe3a31bf32aeec5be4e5fc76dde45c50372
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-15 17:51:09 -0500

    function to add item to cart was configured

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,27 +12,57 @@
     </v-layout>
     </v-container>
    <v-layout row wrap>
-     <v-flex column wrap xs12 sm4 v-for="(promotion,index) in promotions" :key="index">
+     <v-flex column wrap xs12 sm4 lg2 v-for="(promotion,index) in promotions" :key="index">
        <v-card class="mx-auto mt-5"  width="400">
          <v-img :src="promotions[index].imgurl" height="200" width="400" ></v-img> 
-         <v-card-title><h2>{{ promotion.packagename }}</h2></v-card-title>
+         <v-card-title>{{ promotion.packagename }}</v-card-title>
          <v-card-text>Descripcion: {{ promotion.description }}</v-card-text>
          <v-card-text>Precio:$ {{ promotion.price }}</v-card-text>
-         <v-card-actions><v-btn rounded text color="info"> <v-icon>mdi-cart</v-icon> ordenar</v-btn></v-card-actions>
+         <v-card-actions>
+            <v-btn v-if="currentUser" rounded text color="primary" @click="getAItem(promotion.id)"><v-icon>mdi-cart</v-icon>agregar al carrito</v-btn>
+            <v-btn v-if="!currentUser" rounded text color="info" @click="dialog = !dialog"> <v-icon>mdi-cart</v-icon> ordenar</v-btn>
+         </v-card-actions>
        </v-card>
      </v-flex>
    </v-layout>
+
+  <v-dialog v-model="dialog" width="400" persistent>
+      <v-card>
+        <v-card-title>Hey!</v-card-title>
+        <v-card-text>Antes de ordenar debes iniciar sesión</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn rounded text color="primary" @click="dialog=!dialog">Cerrar</v-btn>
+          <v-btn rounded text color="error" to="/register"> registrate </v-btn>
+          <v-btn rounded text color="green" to="/login">iniciar sesión</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-alert type="success" v-model="alert" width="400" class="mx-auto">
+            {{ message }}
+            <v-btn color="blue" text @click="alert=false" class="mx-5">cerrar</v-btn>
+    </v-alert>
  </v-container>
+ 
 </template>
 
 <script>
 import UserService from '../services/user.service';
 import PromotionsService from '../services/promotions.service';
+import promotionsService from '../services/promotions.service';
 export default {
   data:() => ({
     promotions:[],
-    content:''
+    content:'',
+    dialog: false,
+    alert: false,
+    message: ''
   }),
+  computed:{
+    currentUser(){
+      return this.$store.state.auth.user
+    }
+  },
   mounted() {
     UserService.getPublicContent().then(
       response => {
@@ -52,6 +82,23 @@ export default {
 
       }
     );
+  },
+  
+  methods:{
+    getAItem(id){
+      const ID = id;
+      var oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
+      promotionsService.getAPromotion(ID).then(response => {
+        console.log(response.data)
+        oldItems.push(response.data)
+        
+          localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+        
+        this.message = 'Agregado al carrito'
+        this.alert = true
+      })
+     
+    }
   }
 }
 </script>
