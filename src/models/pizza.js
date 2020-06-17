deba276da43d364ecaa61d5b7a26e9ec850dd8e8
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 18:04:45 -0500

    new models were created

---
+++ src/models/pizza.js
@@ -0,0 +1,7 @@
+export default class Pizza{
+    constructor(imgurl,pizzasName,price){
+        this.imgurl = imgurl
+        this.pizzasName = pizzasName
+        this.price = price
+    }
+}
---
+++ src/models/promotion.js
@@ -0,0 +1,8 @@
+export default class Promotion {
+    constructor(imgurl,packagename,description,price){
+        this.imgurl = imgurl
+        this.packagename = packagename
+        this.description = description
+        this.price = price
+    }
+}
