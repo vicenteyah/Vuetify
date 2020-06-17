a24a704918b3146d9838123f7b1e436ba7b54d8f
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 17:47:34 -0500

    pizzas CRUD were configured

--- src/services/pizzas.service.js
+++ src/services/pizzas.service.js
@@ -6,5 +6,22 @@ class PizzasService {
     getPizzas(){
         return axios.get(API_URL + 'pizzas')
     }
+    createPizzas(pizza){
+        return axios.post(API_URL + 'pizzas',{
+            imgurl: pizza.imgurl,
+            pizzasName: pizza.pizzasName,
+            price: pizza.price
+        })
+    }
+    deletePizzas(id){
+        return axios.delete(API_URL + 'pizzas/' + id)
+    }
+    updatePizzas(pizza,id){
+        return axios.put(API_URL +'pizzas/'+id,{
+            imgurl: pizza.imgurl,
+            pizzasName: pizza.pizzasName,
+            price: pizza.price
+        })
+    }
 }
 export default new PizzasService();
