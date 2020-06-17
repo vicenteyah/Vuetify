a5b675c72940ae044e36803bddeac92c08e5fc58
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 18:28:09 -0500

    profile user was modified

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,11 @@
 <template>
     <v-card width="700" class="mx-auto mt-5">
-        <v-card-title><strong>Perfil: {{ currentUser.username }}</strong></v-card-title>
+        <v-card-title><strong>Perfil de: {{ currentUser.username }}</strong></v-card-title>
         <v-card-text><strong>autorización:</strong>
             {{currentUser.accessToken.substring(0, 20)}} ... {{currentUser.accessToken.substr(currentUser.accessToken.length - 20)}}
         </v-card-text>
         <v-card-text>
-            <strong>Id:</strong>
+            <strong>uuid:</strong>
             {{currentUser.id}}
         </v-card-text>
         <v-card-text>
@@ -15,6 +15,9 @@
         <v-card-text v-for="(role,index) in currentUser.roles" :key="index">
             <strong>Role:</strong>{{ role }}
         </v-card-text>
+        <v-card-actions>
+           <v-btn rounded text color="success">ModificarDatos</v-btn>
+        </v-card-actions>
     </v-card>
 </template>
 <script>
