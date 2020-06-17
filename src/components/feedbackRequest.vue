0419da9acb6e400c2baeece5d18abac9197ae5f8
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 18:33:01 -0500

    testing templates

---
+++ src/components/feedbackRequest.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>feedbacks requests</h1>
+    </div>
+</template>
---
+++ src/components/jobExchange.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>
+            jobs exchanges
+        </h2>
+    </div>
+</template>
