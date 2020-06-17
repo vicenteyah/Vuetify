ae0d8195cee38941ca472489ed10a5890671ca5c
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 17:41:20 -0500

    cart component was created and configured

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -10,7 +10,17 @@
                    </v-col>
                    <v-divider></v-divider>
                </v-card>
-               <v-card width="800" class="mx-auto mt-1"  v-for="(item,index) in taskList" :key="index">
+                
+                <v-card v-if="empty" width="800" class="mx-auto mt-1">
+                    <v-col>
+                        <v-row class="mx-auto" justify="center">
+                            <v-card-subtitle> <h3>Tu carrito está vacío</h3></v-card-subtitle>
+                        </v-row>
+                    </v-col>
+                </v-card>
+
+
+               <v-card v-if="!empty" width="800" class="mx-auto mt-1"  v-for="(item,index) in taskList" :key="index">
                     <v-chip label color="primary" text-color="white" class="ml-0">
                        <v-icon left>mdi-label</v-icon>
                         <v-card-title>id del paquete: {{item.id}}</v-card-title>
@@ -21,7 +31,7 @@
                    <v-divider></v-divider>
                    <v-card-actions>
                        <v-spacer></v-spacer>
-                       <v-btn rounded text color="error" @click="deleteItem(item.id)">eliminar</v-btn>
+                       <!--<v-btn rounded text color="error" @click="deleteItem(item.id)">eliminar</v-btn> not working well--> 
                    </v-card-actions>
                </v-card>
            </v-flex>
@@ -31,12 +41,38 @@
                <v-card width="800" class="mx-auto mt-1" >
                    <v-card-title>Total a Comprar:$ {{ Total }}</v-card-title>
                    <v-card-actions>
-                       <v-btn  rounded color="success">comprar carrito</v-btn>
+                       <v-btn  rounded color="success"  @click="dialog=!dialog" >comprar carrito</v-btn>
+                       <v-spacer></v-spacer>
+                       <v-btn rounded  color="info" to="/">regresar a promociones</v-btn>
+                       <v-spacer></v-spacer>
+                       <v-btn rounded color="error" 
+                       :loading="loading" 
+                       :disabled="loading" 
+                       @click="loader = 'loading'"
+                       v-on:click="cancelCart"
+                       >
+                       cancelar carrito</v-btn>
                    </v-card-actions>
                </v-card>
 
            </v-flex>
-       </v-layout>
+        </v-layout>
+
+    <v-dialog v-model="dialog" width="400" persistent>
+      <v-card>
+        <v-card-title>Hey!</v-card-title>
+        <v-card-text>Estas seguro de realizar está compra</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn rounded text color="primary" @click="dialog=!dialog">cancelar</v-btn>
+          <v-btn rounded text color="error" v-on:click="buyNow()" > Si, comprar ahora</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-alert type="success" v-model="alert" width="400" class="mx-auto">
+            Compra realizada con éxito
+            <v-btn color="blue" text @click="alert=false" class="mx-5">cerrar</v-btn>
+     </v-alert>
+    </v-dialog>
     </v-container>
 </template>
 
@@ -47,11 +83,16 @@ export default {
         auxlist:[],
         flag: true,
         Total:0,
+        loading: false,
+        loader: null,
+        empty:true,
+        dialog:false,
+        alert:false
     }),
     computed: {
         currentUser() {
             return this.$store.state.auth.user;
-        }, 
+        }
     },
     mounted() {
         if (!this.currentUser) {
@@ -59,7 +100,11 @@ export default {
         }
         var oldItems = JSON.parse(localStorage.getItem('itemsArray'))
             this.taskList = oldItems
-            
+            if (this.taskList === null){
+                this.empty = true
+            }else{
+                this.empty = false
+            }
             //console.log(this.taskList)
         
            
@@ -71,19 +116,85 @@ export default {
         computeTotalPrice(){
             var oldItems = JSON.parse(localStorage.getItem('itemsArray'))
             this.taskList = oldItems
-            for (let index = 0; index <= this.taskList.length; index++) {
-                console.log(this.taskList)
-                this.Total = this.Total + this.taskList[index].price;  
+            if(this.taskList === null){console.log("there is nothing in cart")}else{ 
+                for (let index = 0; index <= this.taskList.length; index++) {
+                    console.log(this.taskList)
+                    this.Total = this.Total + this.taskList[index].price;  
                 
+                }
             }
             this.flag = false
             return this.Total
+            
         },
         deleteItem(id){
             console.log(id)
             this.taskList = this.taskList.filter(el => el.id != id)
         },
+        cancelCart(){
+            localStorage.removeItem("itemsArray");
+            location.reload()
+            this.empty = true
+        },
+        buyNow(){
+            console.log('has hecho click')
+            var buyItems = JSON.parse(localStorage.getItem('itemsArray'))
+            this.auxlist = buyItems
+            localStorage.setItem('purchases', JSON.stringify(this.auxlist))
+            localStorage.removeItem("itemsArray")
+            this.alert = true
+            location.reload()
+            this.empty = true
+            
+        }
+    },
+    watch:{
+        loader () {
+            const l = this.loader
+            this[l] = !this[l]
+            setTimeout(() => (this[l] = false), 3000)
+            this.loader = null
+        }
     }
-}
+}    
 </script>
+
+<style>
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
 
