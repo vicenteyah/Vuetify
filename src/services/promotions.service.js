88a85a69f4cb30c9bd85f1d6046707acbfeaeb4f
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-17 17:52:58 -0500

    new methods to complete promotions CRUD

--- src/services/promotions.service.js
+++ src/services/promotions.service.js
@@ -9,5 +9,24 @@ class PromotionsService {
     getAPromotion(id){
         return axios.get(API_URL + 'promotions/' + id)
     }
+    createPromotion(promotion){
+        return axios.post(API_URL + 'promotions',{
+            imgurl: promotion.imgurl,
+            packagename: promotion.packagename,
+            description: promotion.description,
+            price: promotion.price
+        })
+    }
+    deletePromotion(id){
+        return axios.delete(API_URL + 'promotions/'+id)
+    }
+    updatePromotion(promotion,id){
+        return axios.put(API_URL + 'promotions/'+id,{
+            imgurl: promotion.imgurl,
+            packagename: promotion.packagename,
+            description: promotion.description,
+            price: promotion.price
+        })
+    }
 }
 export default new PromotionsService();
