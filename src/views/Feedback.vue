b3f089f69df1db2925ce95a16470a7c4472a5a5f
SariMelina <mmkings_beba@hotmail.com>
2020-06-18 05:39:25 -0500

    New view for feedback

---
+++ src/views/Feedback.vue
@@ -0,0 +1,112 @@
+<template>
+    <v-card width="700" class="mx-auto mt-5 mb-3 pa-3">
+        <v-card-title>
+            <h3>Sugerencias</h3>
+        </v-card-title>  
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+            <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="Nombre"
+            required
+            ></v-text-field>
+
+            <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            ></v-text-field>
+
+            <v-text-field
+            label="Teléfono"
+            required
+            ></v-text-field>
+
+            <v-select
+            v-model="select"
+            :items="items"
+            :rules="[v => !!v || 'Item is required']"
+            label="Sucursal"
+            required
+            ></v-select>
+
+            <v-textarea label="Mensaje">
+                
+            </v-textarea>
+
+            <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'Debes de estar de acuerdo para continuar!']"
+            label="¿Estás de acuerdo con el comentario?"
+            required
+            ></v-checkbox>
+
+            <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+            >
+            Validate
+            </v-btn>
+
+            <v-btn
+            color="error"
+            class="mr-4"
+            @click="reset"
+            >
+            Reset Form
+            </v-btn>
+
+            <v-btn
+            color="warning"
+            @click="resetValidation"
+            >
+            Reset Validation
+            </v-btn>
+        </v-form>
+    </v-card>
+</template>
+<script>
+export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Huastechnology Fco. Montejo',
+        'Huastechnology Centro',
+        'Huastechnology Caucel',
+        'Huastechnology Umán',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
