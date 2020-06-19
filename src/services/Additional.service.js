2155238e8645c08e59c8f8e5158293f05703ebf0
vicenteyah <vicente.yah.dzul@icloud.com>
2020-06-19 18:26:47 -0500

    CRUD additional completed

--- src/services/Additional.service.js
+++ src/services/Additional.service.js
@@ -9,6 +9,23 @@ class additionalService {
     getAnAdditional(id){
         return axios.get(API_URL + 'additionals/' + id)
     }
+    createAdditional(additional){
+        return axios.post(API_URL + 'additionals',{
+            imgurl: additional.imgurl,
+            additionalName: additional.additionalName,
+            price: additional.price
+        })
+    }
+    deleteAdditional(id){
+        return axios.delete(API_URL + 'additionals/' + id)
+    }
+    updateAdditional(additional,id){
+        return axios.put(API_URL + 'additionals/' + id , {
+            imgurl: additional.imgurl,
+            additionalName: additional.additionalName,
+            price: additional.price
+        })
+    }
 }
 
 export default new additionalService()
